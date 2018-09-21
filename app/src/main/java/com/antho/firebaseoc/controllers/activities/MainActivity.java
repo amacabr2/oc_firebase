810849f7625cd838367f847af82eb76420ba247a
amacabr2 <anthony.macabrey@edu.univ-fcomte.fr>
2018-09-21 16:33:06 +0200

    Retour identification

--- app/src/main/java/com/antho/firebaseoc/controllers/activities/MainActivity.java
+++ app/src/main/java/com/antho/firebaseoc/controllers/activities/MainActivity.java
@@ -1,24 +1,32 @@
 package com.antho.firebaseoc.controllers.activities;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import android.content.Intent;
+
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.Snackbar;
 
 import com.antho.firebaseoc.R;
-import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.ErrorCodes;
+import com.firebase.ui.auth.IdpResponse;
 
 import java.util.Arrays;
 
+import butterknife.BindView;
 import butterknife.OnClick;
 
-import static com.firebase.ui.auth.AuthUI.*;
+import static com.firebase.ui.auth.AuthUI.IdpConfig;
+import static com.firebase.ui.auth.AuthUI.getInstance;
 
 public class MainActivity extends BaseActivity {
 
     private static final int RC_SIGN_IN = 123;
 
+    @BindView(R.id.main_activity_coordinator_layout)
+    CoordinatorLayout coordinatorLayout;
+
     @Override
     public int getFragmentLayout() {
-        return 0;
+        return R.layout.activity_main;
     }
 
     @OnClick(R.id.main_activity_button_login)
@@ -29,13 +37,41 @@ public class MainActivity extends BaseActivity {
     public void startSignInActivity() {
         startActivityForResult(
                 getInstance()
-                    .createSignInIntentBuilder()
-                    .setTheme(R.style.LoginTheme)
-                    .setAvailableProviders(Arrays.asList(new IdpConfig.Builder(EMAIL_PROVIDER).build()))
-                    .setIsSmartLockEnabled(false, true)
-                    .setLogo(R.drawable.ic_logo_auth)
-                    .build(),
+                        .createSignInIntentBuilder()
+                        .setTheme(R.style.LoginTheme)
+                        .setAvailableProviders(Arrays.asList((new IdpConfig.EmailBuilder()).build()))
+                        .setIsSmartLockEnabled(false, true)
+                        .setLogo(R.drawable.ic_logo_auth)
+                        .build(),
                 RC_SIGN_IN
         );
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        this.handleResponseAfterSignIn(requestCode, resultCode, data);
+    }
+
+    private void showSnackBar(CoordinatorLayout coordinatorLayout, String message) {
+        Snackbar.make(coordinatorLayout, message, Snackbar.LENGTH_SHORT).show();
+    }
+
+    private void handleResponseAfterSignIn(int requestCode, int resultCode, Intent data) {
+        IdpResponse response = IdpResponse.fromResultIntent(data);
+
+        if (requestCode == RC_SIGN_IN) {
+            if (resultCode == RESULT_OK) { // SUCCESS
+                showSnackBar(this.coordinatorLayout, getString(R.string.connection_succeed));
+            } else { // ERRORS
+                if (response == null) {
+                    showSnackBar(this.coordinatorLayout, getString(R.string.error_authentication_canceled));
+                } else if (response.getError().getErrorCode() == ErrorCodes.NO_NETWORK) {
+                    showSnackBar(this.coordinatorLayout, getString(R.string.error_no_internet));
+                } else if (response.getError().getErrorCode() == ErrorCodes.UNKNOWN_ERROR) {
+                    showSnackBar(this.coordinatorLayout, getString(R.string.error_unknown_error));
+                }
+            }
+        }
     }
 }
