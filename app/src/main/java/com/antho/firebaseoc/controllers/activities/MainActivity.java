a00ac452031380384c970d5e582eb19bda7f770d
amacabr2 <anthony.macabrey@edu.univ-fcomte.fr>
2018-09-21 15:46:06 +0200

    Authentification par email

--- app/src/main/java/com/antho/firebaseoc/controllers/activities/MainActivity.java
+++ app/src/main/java/com/antho/firebaseoc/controllers/activities/MainActivity.java
@@ -4,8 +4,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.antho.firebaseoc.R;
+import com.firebase.ui.auth.AuthUI;
+
+import java.util.Arrays;
 
 import butterknife.OnClick;
+
+import static com.firebase.ui.auth.AuthUI.*;
 
 public class MainActivity extends BaseActivity {
 
@@ -16,6 +21,21 @@ public class MainActivity extends BaseActivity {
         return 0;
     }
 
+    @OnClick(R.id.main_activity_button_login)
+    public void onClickLoginButton() {
+        this.startSignInActivity();
+    }
 
-
+    public void startSignInActivity() {
+        startActivityForResult(
+                getInstance()
+                    .createSignInIntentBuilder()
+                    .setTheme(R.style.LoginTheme)
+                    .setAvailableProviders(Arrays.asList(new IdpConfig.Builder(EMAIL_PROVIDER).build()))
+                    .setIsSmartLockEnabled(false, true)
+                    .setLogo(R.drawable.ic_logo_auth)
+                    .build(),
+                RC_SIGN_IN
+        );
+    }
 }
