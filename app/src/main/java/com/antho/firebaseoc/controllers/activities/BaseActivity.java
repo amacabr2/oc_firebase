bae0bfd210326d6451b46db2dbaec1c7a085eb67
amacabr2 <anthony.macabrey@edu.univ-fcomte.fr>
2018-09-21 14:34:24 +0200

    Classe de parente pour les "activities"

---
+++ app/src/main/java/com/antho/firebaseoc/controllers/activities/BaseActivity.java
@@ -0,0 +1,33 @@
+package com.antho.firebaseoc.controllers.activities;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+
+import butterknife.ButterKnife;
+
+public abstract class BaseActivity extends AppCompatActivity {
+
+    // --------------------
+    // LIFE CYCLE
+    // --------------------
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.setContentView(this.getFragmentLayout());
+        ButterKnife.bind(this); //Configure Butterknife
+    }
+
+    public abstract int getFragmentLayout();
+
+    // --------------------
+    // UI
+    // --------------------
+
+    protected void configureToolbar(){
+        ActionBar ab = getSupportActionBar();
+        ab.setDisplayHomeAsUpEnabled(true);
+    }
+}
--- app/src/main/java/com/antho/firebaseoc/controllers/activities/MainActivity.java
+++ app/src/main/java/com/antho/firebaseoc/controllers/activities/MainActivity.java
@@ -1,13 +1,20 @@
-package com.antho.firebaseoc;
+package com.antho.firebaseoc.controllers.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.antho.firebaseoc.R;
+
+public class MainActivity extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public int getFragmentLayout() {
+        return 0;
     }
 }
