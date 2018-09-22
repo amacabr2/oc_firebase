1edd208fad6d5f415c1a4370189fc609a5962969
amacabr2 <anthony.macabrey@edu.univ-fcomte.fr>
2018-09-22 13:39:13 +0200

    Template pour afficher le chat

---
+++ app/src/main/java/com/antho/firebaseoc/controllers/activities/MentorChatActivity.java
@@ -0,0 +1,13 @@
+package com.antho.firebaseoc.controllers.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MentorChatActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mentor_chat);
+    }
+}
