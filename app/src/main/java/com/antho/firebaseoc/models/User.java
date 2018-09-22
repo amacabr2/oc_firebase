bafaa4a39659dbf8e5bd9f4adc9448b16cfbbd14
amacabr2 <anthony.macabrey@edu.univ-fcomte.fr>
2018-09-22 12:02:17 +0200

    Implémentation des données (model User et Message)

---
+++ app/src/main/java/com/antho/firebaseoc/models/User.java
@@ -0,0 +1,54 @@
+package com.antho.firebaseoc.models;
+
+import android.support.annotation.Nullable;
+
+public class User {
+
+    private String uid;
+    private String username;
+    private boolean isMentor;
+    @Nullable private String urlPicture;
+
+    public User() {
+    }
+
+    public User(String uid, String username, @Nullable String urlPicture) {
+        this.uid = uid;
+        this.username = username;
+        this.isMentor = false;
+        this.urlPicture = urlPicture;
+    }
+
+    public String getUid() {
+        return uid;
+    }
+
+    public void setUid(String uid) {
+        this.uid = uid;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public boolean isMentor() {
+        return isMentor;
+    }
+
+    public void setMentor(boolean mentor) {
+        isMentor = mentor;
+    }
+
+    @Nullable
+    public String getUrlPicture() {
+        return urlPicture;
+    }
+
+    public void setUrlPicture(@Nullable String urlPicture) {
+        this.urlPicture = urlPicture;
+    }
+}
