72684ebd67d6527c24ec8deb86dcaed361c2a2e5
amacabr2 <anthony.macabrey@edu.univ-fcomte.fr>
2018-09-22 11:16:54 +0200

    Implémentation de la déconnexion et de la suppression d'un compte

--- app/src/main/java/com/antho/firebaseoc/controllers/activities/ProfileActivity.java
+++ app/src/main/java/com/antho/firebaseoc/controllers/activities/ProfileActivity.java
@@ -1,6 +1,8 @@
 package com.antho.firebaseoc.controllers.activities;
 
+import android.content.DialogInterface;
 import android.support.design.widget.TextInputEditText;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
@@ -11,6 +13,8 @@ import android.widget.TextView;
 import com.antho.firebaseoc.R;
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.request.RequestOptions;
+import com.firebase.ui.auth.AuthUI;
+import com.google.android.gms.tasks.OnSuccessListener;
 
 import butterknife.BindView;
 import butterknife.OnClick;
@@ -25,6 +29,9 @@ public class ProfileActivity extends BaseActivity {
     TextView textViewEmail;
     @BindView(R.id.profile_activity_progress_bar)
     ProgressBar progressBar;
+
+    private static final int SIGN_OUT_TASK = 10;
+    private static final int DELETE_USER_TASK = 20;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,13 +50,28 @@ public class ProfileActivity extends BaseActivity {
     // --------------------
 
     @OnClick(R.id.profile_activity_button_update)
-    public void onClickUpdateButton() { }
+    public void onClickUpdateButton() {
+
+    }
 
     @OnClick(R.id.profile_activity_button_sign_out)
-    public void onClickSignOutButton() { }
+    public void onClickSignOutButton() {
+        this.signOutUserFromFirebase();
+    }
 
     @OnClick(R.id.profile_activity_button_delete)
-    public void onClickDeleteButton() { }
+    public void onClickDeleteButton() {
+        new AlertDialog.Builder(this)
+                .setMessage(R.string.popup_message_confirmation_delete_account)
+                .setPositiveButton(R.string.popup_message_choice_yes, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        deleteUserFromFirebase();
+                    }
+                })
+                .setNegativeButton(R.string.popup_message_choice_no, null)
+                .show();
+    }
 
     // --------------------
     // UI
@@ -69,6 +91,42 @@ public class ProfileActivity extends BaseActivity {
 
             this.textInputEditTextUsername.setText(username);
             this.textViewEmail.setText(email);
+        }
+    }
+
+    private OnSuccessListener<Void> updateUIAfterRESTRequestsCompleted(final int origin){
+        return new OnSuccessListener<Void>() {
+            @Override
+            public void onSuccess(Void aVoid) {
+                switch (origin){
+                    case SIGN_OUT_TASK:
+                        finish();
+                        break;
+                    case DELETE_USER_TASK:
+                        finish();
+                        break;
+                    default:
+                        break;
+                }
+            }
+        };
+    }
+
+    // --------------------
+    // REST REQUESTS
+    // --------------------
+
+    private void signOutUserFromFirebase() {
+        AuthUI.getInstance()
+                .signOut(this)
+                .addOnSuccessListener(this, this.updateUIAfterRESTRequestsCompleted(SIGN_OUT_TASK));
+    }
+
+    private void deleteUserFromFirebase(){
+        if (this.getCurrentUser() != null) {
+            AuthUI.getInstance()
+                    .delete(this)
+                    .addOnSuccessListener(this, this.updateUIAfterRESTRequestsCompleted(DELETE_USER_TASK));
         }
     }
 }
